b00dff4acdf3097ab874ed5ab3b931248d1a5fae
xr0m <xr0m@yandex.ru>
2018-03-06 02:30:17 +0200

    dist

--- dist/emailjs-com.d.ts
+++ dist/emailjs-com.d.ts
@@ -1,21 +1,45 @@
-import 'whatwg-fetch';
-import 'promise-polyfill';
 export declare class EmailJS {
-    private _URL;
     private _userID;
+    private sendPost(url, data, headers?);
+    private appendGoogleCaptcha(templatePrams?);
     /**
      * Initiation
      * @param {string} userID - set the emailJS user ID
      */
     init(userID: string): void;
     /**
-     * Send a template to specific server ID
-     * @param {string} serverID - the emailJS server ID
-     * @param {string} templateID - the emailJS template ID
-     * @param {Object} templatePrams - the template params, what will be set to the emailJS template
-     * @param {string} userID - the emailJS user ID
-     * @returns {<Response>}
+     * Send a template to the specific EmailJS service
+     * @param {string} serviceID - the EmailJS service ID
+     * @param {string} templateID - the EmailJS template ID
+     * @param {Object} templatePrams - the template params, what will be set to the EmailJS template
+     * @param {string} userID - the EmailJS user ID
+     * @returns {Promise<EmailJSResponseStatus>}
      */
-    send(serverID: string, templateID: string, templatePrams?: Object, userID?: string): Promise<Response>;
+    send(serviceID: string, templateID: string, templatePrams?: Object, userID?: string): Promise<EmailJSResponseStatus>;
+    /**
+     * Send a form the specific EmailJS service
+     * @param {string} serviceID - the emailJS service ID
+     * @param {string} templateID - the emailJS template ID
+     * @param {string | HTMLFormElement} form - the form element or selector
+     * @param {string} userID - the emailJS user ID
+     * @returns {Promise<EmailJSResponseStatus>}
+     */
+    sendForm(serviceID: string, templateID: string, form: string | HTMLFormElement, userID?: string): Promise<EmailJSResponseStatus>;
 }
 export declare const emailjs: EmailJS;
+
+export declare class EmailJSResponseStatus {
+    status: number;
+    text: string;
+    constructor(httpResponse: XMLHttpRequest);
+}
+
+export declare class UI {
+    private static readonly PROGRESS;
+    private static readonly DONE;
+    private static readonly ERROR;
+    static clearAll(form: HTMLFormElement): void;
+    static progressState(form: HTMLFormElement): void;
+    static successState(form: HTMLFormElement): void;
+    static errorState(form: HTMLFormElement): void;
+}
