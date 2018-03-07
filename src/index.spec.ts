0bbefa4313369af0f31f2025e6bc95b41212e9f4
xr0m <xr0m@yandex.ru>
2018-03-07 20:10:57 +0200

    Test: Added success steps

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -17,6 +17,22 @@ test ('send method should call error', () => {
     });
 });
 
+test ('send method should be success', () => {
+  expect.assertions(1);
+
+  return emailjs.send('test_service', 'my_test_template', {
+    reply_to: 'support@emailjs.com',
+    to_name: 'Tester',
+    from_name: 'JEST',
+    message_html: '<span style="color:#ff5500">Looks like this test is passed</span>'
+  } , 'user_LC2JWGNosRSeMY6HmQFUn')
+    .then((resolve) => {
+      expect(resolve).toEqual({'status': 200, 'text': 'OK'});
+    }, (error) => {
+      expect(error).toBeUndefined();
+    });
+});
+
 test ('sendForm method form element validation', () => {
   expect(() => emailjs.sendForm('test', 'test', '.form-not-exist'))
     .toThrow('Expected the HTML form element or the style selector of form');
@@ -31,6 +47,23 @@ test ('sendForm method should call error', () => {
       expect(resolve).toBeUndefined();
     }, (error) => {
       expect(error).toBeDefined();
+    });
+});
+
+test ('sendForm method should be success', () => {
+  let form: HTMLFormElement = <HTMLFormElement>document.createElement('FORM');
+  let username: HTMLInputElement = <HTMLInputElement>document.createElement('INPUT');
+  username.name = 'username';
+  username.value = 'JEST';
+  form.appendChild(username);
+
+  expect.assertions(1);
+
+  return emailjs.sendForm('test_service', 'my_form_template', form, 'user_LC2JWGNosRSeMY6HmQFUn')
+    .then((resolve) => {
+      expect(resolve).toEqual({'status': 200, 'text': 'OK'});
+    }, (error) => {
+      expect(error).toBeUndefined();
     });
 });
 
