ec74d2fb3461c50197e2141fe58c81a307f85f86
xr0m <xr0m@yandex.ru>
2018-11-25 23:19:47 +0200

    Build: 2.3.1

--- source/index.js
+++ source/index.js
@@ -55,7 +55,7 @@ exports.init = init;
  */
 function send(serviceID, templateID, templatePrams, userID) {
     var params = {
-        lib_version: '2.3.0',
+        lib_version: '2.3.1',
         user_id: userID || _userID,
         service_id: serviceID,
         template_id: templateID,
@@ -83,7 +83,7 @@ function sendForm(serviceID, templateID, form, userID) {
     }
     UI_1.UI.progressState(form);
     var formData = new FormData(form);
-    formData.append('lib_version', '2.3.0');
+    formData.append('lib_version', '2.3.1');
     formData.append('service_id', serviceID);
     formData.append('template_id', templateID);
     formData.append('user_id', userID || _userID);
