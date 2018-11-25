45c54ca0034b9886f2ccb1afe5de4c8529338475
xr0m <xr0m@yandex.ru>
2018-11-25 23:29:49 +0200

    Build: 2.3.2

--- dist/email.js
+++ dist/email.js
@@ -611,7 +611,7 @@ exports.init = init;
  */
 function send(serviceID, templateID, templatePrams, userID) {
     var params = {
-        lib_version: '2.3.1',
+        lib_version: '2.3.2',
         user_id: userID || _userID,
         service_id: serviceID,
         template_id: templateID,
@@ -639,7 +639,7 @@ function sendForm(serviceID, templateID, form, userID) {
     }
     UI_1.UI.progressState(form);
     var formData = new FormData(form);
-    formData.append('lib_version', '2.3.1');
+    formData.append('lib_version', '2.3.2');
     formData.append('service_id', serviceID);
     formData.append('template_id', templateID);
     formData.append('user_id', userID || _userID);
