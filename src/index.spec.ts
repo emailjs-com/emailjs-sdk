70bdca02bfa04204c16e28825edcda0be88f0552
Sergey Khomushin <sergey@placer.io>
2020-07-20 19:28:41 +0300

    index.spec: fix specs, add new test for id

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,37 +1,32 @@
 import emailjs from './index';
 
-test ('send method should call error', () => {
-  expect.assertions(1);
-
-  return emailjs.send('test', 'test')
+it('should send method and fail', (done) => {
+  emailjs.send('test', 'test')
     .then((resolve) => {
       expect(resolve).toBeUndefined();
     }, (error) => {
       expect(error).toBeDefined();
-    });
-});
+    })
+    .finally(done);
+}, 10000);
 
-test ('init and send method should be success', () => {
-  expect.assertions(1);
-
+it('should init and send method successfully', (done) => {
   emailjs.init('user_LC2JWGNosRSeMY6HmQFUn');
 
-  return emailjs.send('default_service', 'my_test_template', {
+  emailjs.send('default_service', 'my_test_template', {
     reply_to: 'support@emailjs.com',
     to_name: 'Tester',
     from_name: 'JEST',
     message_html: '<span style="color:#ff5500">Looks like this test is passed</span>'
   }).then((resolve) => {
-      expect(resolve).toEqual({'status': 200, 'text': 'OK'});
-    }, (error) => {
-      expect(error).toBeUndefined();
-    });
-});
+    expect(resolve).toEqual({'status': 200, 'text': 'OK'});
+  }, (error) => {
+    expect(error).toBeUndefined();
+  }).finally(done);
+}, 10000);
 
-test ('send method should be success', () => {
-  expect.assertions(1);
-
-  return emailjs.send('default_service', 'my_test_template', {
+it('should send method successfully', (done) => {
+  emailjs.send('default_service', 'my_test_template', {
     reply_to: 'support@emailjs.com',
     to_name: 'Tester',
     from_name: 'JEST',
@@ -41,39 +36,53 @@ test ('send method should be success', () => {
       expect(resolve).toEqual({'status': 200, 'text': 'OK'});
     }, (error) => {
       expect(error).toBeUndefined();
-    });
-});
+    })
+    .finally(done);
+}, 10000);
 
-test ('sendForm method form element validation', () => {
-  expect(() => emailjs.sendForm('test', 'test', '.form-not-exist'))
+it('should call sendForm method form element validation', () => {
+  expect(() => emailjs.sendForm('test', 'test', 'form-not-exist'))
     .toThrow('Expected the HTML form element or the style selector of form');
 });
 
-test ('sendForm method should call error', () => {
-  let form: HTMLFormElement = <HTMLFormElement>document.createElement('FORM');
-  expect.assertions(1);
+it('should call sendForm with non id selector', (done) => {
+  let form: HTMLFormElement = document.createElement('form');
+  form.id = 'form-id';
+  document.body.appendChild(form);
 
-  return emailjs.sendForm('test', 'test', form)
+  emailjs.sendForm('test', 'test', 'form-id')
     .then((resolve) => {
       expect(resolve).toBeUndefined();
     }, (error) => {
       expect(error).toBeDefined();
-    });
-});
+    })
+    .finally(done);
+}, 10000);
 
-test ('sendForm method should be success', () => {
-  let form: HTMLFormElement = <HTMLFormElement>document.createElement('FORM');
-  let username: HTMLInputElement = <HTMLInputElement>document.createElement('INPUT');
+it('should call sendForm method and fail', (done) => {
+  let form: HTMLFormElement = document.createElement('form');
+
+  emailjs.sendForm('test', 'test', form)
+    .then((resolve) => {
+      expect(resolve).toBeUndefined();
+    }, (error) => {
+      expect(error).toBeDefined();
+    })
+    .finally(done);
+}, 10000);
+
+it('should call sendForm method successfully', (done) => {
+  let form: HTMLFormElement = document.createElement('form');
+  let username: HTMLInputElement = document.createElement('input');
   username.name = 'username';
   username.value = 'JEST';
   form.appendChild(username);
 
-  expect.assertions(1);
-
-  return emailjs.sendForm('default_service', 'my_test_template', form, 'user_LC2JWGNosRSeMY6HmQFUn')
+  emailjs.sendForm('default_service', 'my_test_template', form, 'user_LC2JWGNosRSeMY6HmQFUn')
     .then((resolve) => {
       expect(resolve).toEqual({'status': 200, 'text': 'OK'});
     }, (error) => {
       expect(error).toBeUndefined();
-    });
-});
+    })
+    .finally(done);
+}, 10000);
