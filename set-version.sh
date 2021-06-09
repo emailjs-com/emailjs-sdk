740111a916ba745642f36d02092b7e078e678bcb
Sergey Khomushin <sergey@placer.io>
2021-06-09 15:46:52 +0300

    set-version.sh: fix the text

--- set-version.sh
+++ set-version.sh
@@ -3,4 +3,4 @@
 sed -i '' -e "s/process.env.npm_package_version/'$1'/g" ./es/methods/send/send.js
 sed -i '' -e "s/process.env.npm_package_version/'$1'/g" ./es/methods/sendForm/sendForm.js
 
-echo -e "\033[0;33mApplication version is $1\033[0m"
+echo -e "\033[0;33mSDK version is $1\033[0m"
