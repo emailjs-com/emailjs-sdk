cd5905f292d53e6385707bf214cbc1bf3fd4c4d2
Sergey Khomushin <sergey@placer.io>
2021-07-09 23:42:47 +0300

    set-version.sh: add the dir param

--- set-version.sh
+++ set-version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed -i '' -e "s/process.env.npm_package_version/'$1'/g" ./es/methods/send/send.js
-sed -i '' -e "s/process.env.npm_package_version/'$1'/g" ./es/methods/sendForm/sendForm.js
+sed -i '' -e "s/process.env.npm_package_version/'$1'/g" "./$2/methods/send/send.js"
+sed -i '' -e "s/process.env.npm_package_version/'$1'/g" "./$2/methods/sendForm/sendForm.js"
 
 echo -e "\033[0;33mSDK version is $1\033[0m"
