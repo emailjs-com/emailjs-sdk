0b4e2183f5aac439fb0f5efe10bc88946edaa267
Sergey Khomushin <xr0master@gmail.com>
2024-01-24 22:14:46 +0800

    fix typo

--- src/methods/send/send.ts
+++ src/methods/send/send.ts
@@ -8,14 +8,14 @@ import type { EmailJSResponseStatus } from '../../models/EmailJSResponseStatus';
  * Send a template to the specific EmailJS service
  * @param {string} serviceID - the EmailJS service ID
  * @param {string} templateID - the EmailJS template ID
- * @param {object} templatePrams - the template params, what will be set to the EmailJS template
+ * @param {object} templateParams - the template params, what will be set to the EmailJS template
  * @param {string} publicKey - the EmailJS public key
  * @returns {Promise<EmailJSResponseStatus>}
  */
 export const send = (
   serviceID: string,
   templateID: string,
-  templatePrams?: Record<string, unknown>,
+  templateParams?: Record<string, unknown>,
   publicKey?: string,
 ): Promise<EmailJSResponseStatus> => {
   const uID = publicKey || store._userID;
@@ -27,7 +27,7 @@ export const send = (
     user_id: uID,
     service_id: serviceID,
     template_id: templateID,
-    template_params: templatePrams,
+    template_params: templateParams,
   };
 
   return sendPost('/api/v1.0/email/send', JSON.stringify(params), {
