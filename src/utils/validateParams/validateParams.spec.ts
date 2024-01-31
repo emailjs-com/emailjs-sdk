175811b95d06daaccfb94224b65127dbf26005d3
Sergey Khomushin <xr0master@gmail.com>
2024-01-31 15:13:10 +0800

    validateParams: add type validation

--- src/utils/validateParams/validateParams.spec.ts
+++ src/utils/validateParams/validateParams.spec.ts
@@ -1,22 +1,46 @@
-import { it, expect } from '@jest/globals';
+import { it, expect, describe } from '@jest/globals';
 import { validateParams } from './validateParams';
 
-it('should fail on the public key', () => {
-  expect(() => validateParams('', 'default_service', 'my_test_template')).toThrow(
-    'The public key is required',
-  );
+describe('should fail on the public key', () => {
+  it('no key', () => {
+    expect(() => validateParams('', 'default_service', 'my_test_template')).toThrow(
+      'The public key is required',
+    );
+  });
+
+  it('invalida type', () => {
+    expect(() => validateParams({}, 'default_service', 'my_test_template')).toThrow(
+      'The public key is required',
+    );
+  });
 });
 
-it('should fail on the service ID', () => {
-  expect(() => validateParams('d2JWGTestKeySomething', '', 'my_test_template')).toThrow(
-    'The service ID is required',
-  );
+describe('should fail on the service ID', () => {
+  it('no key', () => {
+    expect(() => validateParams('d2JWGTestKeySomething', '', 'my_test_template')).toThrow(
+      'The service ID is required',
+    );
+  });
+
+  it('invalida type', () => {
+    expect(() => validateParams('d2JWGTestKeySomething', [], 'my_test_template')).toThrow(
+      'The service ID is required',
+    );
+  });
 });
 
-it('should fail on the template ID', () => {
-  expect(() => validateParams('d2JWGTestKeySomething', 'default_service', '')).toThrow(
-    'The template ID is required',
-  );
+describe('should fail on the template ID', () => {
+  it('no key', () => {
+    expect(() => validateParams('d2JWGTestKeySomething', 'default_service', '')).toThrow(
+      'The template ID is required',
+    );
+  });
+
+  it('invalida type', () => {
+    expect(() => validateParams('d2JWGTestKeySomething', 'default_service', 3)).toThrow(
+      'The template ID is required',
+    );
+  });
 });
 
 it('should successfully pass the validation', () => {
