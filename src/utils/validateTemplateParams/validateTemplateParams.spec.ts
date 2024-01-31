a0bb1e0f2686fe7e1d8673b95d8d915d4b29fc7d
Sergey Khomushin <xr0master@gmail.com>
2024-01-31 15:27:35 +0800

    validateTemplateParams: improve type

--- src/utils/validateTemplateParams/validateTemplateParams.spec.ts
+++ src/utils/validateTemplateParams/validateTemplateParams.spec.ts
@@ -2,12 +2,10 @@ import { it, expect } from '@jest/globals';
 import { validateTemplateParams } from './validateTemplateParams';
 
 it('should fail on wrong type', () => {
-  expect(() => validateTemplateParams('variable' as unknown as Record<string, unknown>)).toThrow(
+  expect(() => validateTemplateParams('variable')).toThrow(
     'The template params have to be the object.',
   );
-  expect(() => validateTemplateParams([] as unknown as Record<string, unknown>)).toThrow(
-    'The template params have to be the object.',
-  );
+  expect(() => validateTemplateParams([])).toThrow('The template params have to be the object.');
 });
 
 it('should successfully pass the validation', () => {
--- src/utils/validateTemplateParams/validateTemplateParams.ts
+++ src/utils/validateTemplateParams/validateTemplateParams.ts
@@ -1,4 +1,4 @@
-export const validateTemplateParams = (templateParams?: Record<string, unknown>) => {
+export const validateTemplateParams = (templateParams?: unknown) => {
   // eslint-disable-next-line @typescript-eslint/no-base-to-string
   if (templateParams && templateParams.toString() !== '[object Object]') {
     throw 'The template params have to be the object. Visit https://www.emailjs.com/docs/sdk/send/';
